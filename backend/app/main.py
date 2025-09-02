6e693448bec5da1a065699cb13835f78db6c5361
Prosper Raheem <prosperraheem6@gmail.com>
2025-09-02 21:03:39 +0100

    Pydantic models Initial creation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from models import AnalyseRequest, AnalyseResponse
 
 app = FastAPI()
 
 @app.get("/") # this is the root endpoint and it will be called when we access the root URL which is http://localhost:8000/ this root URL is also called the home page and this is the first page that will be displayed when we access the URL
 def read_root():
     return {"Hello": "Prospero"} # this will return a JSON response with a key "Hello" and value "World" on accessing the root URL
+
+@app.post("/analyse", response_model=AnalyseResponse) 
+def handle_analysis(request: AnalyseRequest) -> AnalyseResponse:
+    # For the MVP, we imagine that a CV and job link was provided in the request
+    # and we would process them to generate an analysis response.
+    
+    if request.job_link and request.cv_text:
+        """
+        Here, we would implement the actual logic to:
+        1: Scrape the job link to get the job description
+        2: Perform deep Retrieval-Augmented Generation (RAG) analysis using the CV and job information
+        3: Return the full AnalyseResponse JSON object which includes match_score, analysis_summary, and suggested_improvements
+        """
+        pass
+    else:
+        # If either the job link or CV text is missing, we return a default response
+        return AnalyseResponse(
+            match_score=0.0,
+            analysis_summary="Please a valid job link and CV text to get an analysis.",
+            suggested_improvements=[]
+        )
+
+
+        
+
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,29 @@
 # Definition of the pydantic models used in the application
+
+from pydantic import BaseModel #  Pydantic is a data validation and settings management library, we import BaseModel to create data models
+from typing import List, Optional # Typing is a standard library module for type hints in Python and we import List and Optional for type annotations
+
+"""
+this model defines the shape of the JSON data that will be sent to the backend
+when a user makes an analysis POST request to the /analyse endpoint from the frontend
+class AnalyseRequest(BaseModel):
+"""
+class AnalyseRequest(BaseModel):
+    cv_text: str
+    job_link: Optional[str] = None
+    initial_prompt: Optional[str] = None
+
+
+"""
+the below models defines the shape of the JSON data that will be sent back to the frontend
+when the backend responds to an analysis request made to the /analyse endpoint
+"""
+class ImprovementSuggestion(BaseModel):
+    original_text: str 
+    suggested_text: str 
+    improvement_reason: str
+
+class AnalyseResponse(BaseModel):
+    match_score: float
+    analysis_summary: str 
+    suggested_improvements: List[ImprovementSuggestion]
